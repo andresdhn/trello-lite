25086ced68accad3d891dc7a8c5e73f4630a44d7
Andres <hernandez.andres.d@gmail.com>
2020-02-10 16:30:28 +1100

    Updates

---
+++ precache-manifest.f265fe594c3b77ee6b347eee5b3ceb4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a16beccc4996ee4e49f156e6a4a7ff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f72ac776b21c8328f2d",
+    "url": "/static/js/2.b269d495.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b269d495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39d519695cde32ecf09",
+    "url": "/static/js/main.a61c8002.chunk.js"
+  },
+  {
+    "revision": "f66902466b0341315d7c",
+    "url": "/static/js/runtime-main.376ad986.js"
+  }
+]);
