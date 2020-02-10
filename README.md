f04379cd3c54b65693c2d3a0f718a4fee46036ff
Andres <hernandez.andres.d@gmail.com>
2020-02-10 16:28:47 +1100

    Update dependencies + creates README file + fixes old React Warning

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trello-lite
+
+Trello-lite is a client-side SPA based on the popular 'Kanban' project admin tool [https://trello.com](https://trello.com)
+
+This project was NOT bootstrapped with create-react-app to understand the basics of the whole react project configuration.
+
+Trello-lite was one of the first web applications I built whilst learning the Framework. Hope you enjoy it. I plan to expand the functionalities in the short future.
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,67 +1,64 @@
-
-import React 	from 'react';
+import React from "react";
 
 class Todo extends React.Component {
-	constructor(props) {
-		super(props); 
+  constructor(props) {
+    super(props);
 
-		this.state ={
-			'editing': true
-		}
-	}
+    this.state = {
+      editing: true
+    };
+  }
 
-	componentDidMount() {
-		this.selectText()
-	}
+  componentDidMount() {
+    this.selectText();
+  }
 
-	componentDidUpdate() {
-		this.selectText()		
-	}
+  componentDidUpdate() {
+    this.selectText();
+  }
 
+  edit() {
+    this.setState({ editing: true });
+  }
 
-	edit(){
-		this.setState({ 'editing': true })
-	}
+  save() {
+    this.props.onSave(this.props.id, this.refs.todo.value);
+    this.setState({ editing: false });
+  }
 
-	save(){
-		this.props.onSave ( this.props.id, this.refs.todo.value )
-		this.setState({ 'editing': false })
-	}
+  selectText() {
+    if (this.state.editing) {
+      this.refs.todo.focus();
+      this.refs.todo.select();
+    }
+  }
 
-	selectText() {
-		if (this.state.editing) {
-			this.refs.todo.focus()
-			this.refs.todo.select()
-		}
-	}
+  renderControl() {
+    return (
+      <input
+        type="text"
+        className="form-control"
+        ref="todo"
+        onBlur={this.save.bind(this)}
+        defaultValue={this.props.children}
+      />
+    );
+  }
 
-	renderControl() {
-		return(
-			<input type="text" 
-				className="form-control" 
-				ref="todo" 
-				onBlur = { this.save.bind(this) }
-				defaultValue={ this.props.children }
-			/>
-		)
-	}
+  renderDisplay() {
+    return (
+      <div
+        className="list-group-item list-group-item-action"
+        onClick={this.edit.bind(this)}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
 
-	renderDisplay() {
-		return(
-			<a href="#" 
-				className="list-group-item list-group-item-action"
-				onClick={ this.edit.bind(this) }>
-				{ this.props.children }
-			</a>
-		)
-	}
-
-
-	render(){
-		return(
-			(this.state.editing) ? this.renderControl() : this.renderDisplay()		
-		) 
-	}
+  render() {
+    return this.state.editing ? this.renderControl() : this.renderDisplay();
+  }
 }
 
 export default Todo;
