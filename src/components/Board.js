e2b023c75f539c61faa0667f4404ed72f2e3bc30
Andres <hernandez.andres.d@gmail.com>
2020-02-10 17:25:35 +1100

    Fixes mobile size + responsive font-size

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,55 +1,65 @@
-
-import React 	from 'react';
-import List 	from './List';
+import React from "react";
+import List from "./List";
 
 class Board extends React.Component {
-	constructor(props){
-		super(props)
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			'lists': [
-				{'id': 0, 'name':'...'}
-			]
-		}
-	}
+    this.state = {
+      lists: [{ id: 0, name: "..." }]
+    };
+  }
 
-	nextId() {
-		let newId = this.state.lists.length;
-        return newId++;
-	}
+  nextId() {
+    let newId = this.state.lists.length;
+    return newId++;
+  }
 
-	addList() {
-		let newList = this.state.lists; 
-		newList.push( { 'id': this.nextId(), 'name':'...', 'editing': true } ); 
-		this.setState ( newList );
-	}
+  addList() {
+    let newList = this.state.lists;
+    newList.push({ id: this.nextId(), name: "...", editing: true });
+    this.setState(newList);
+  }
 
-	save(newName, id) {
-		let lists = this.state.lists.map( list => (list.id !== id) ? list : { 'id': id, 'name': newName }); 
-		this.setState({ lists });
-	}
+  save(newName, id) {
+    let lists = this.state.lists.map(list =>
+      list.id !== id ? list : { id: id, name: newName }
+    );
+    this.setState({ lists });
+  }
 
-	renderList(list){
-		return(
-			<List key={ list.id } id={ list.id } name={ list.name } onSave={ this.save.bind(this) }></List>
-		)
-	}
+  renderList(list) {
+    return (
+      <List
+        key={list.id}
+        id={list.id}
+        name={list.name}
+        onSave={this.save.bind(this)}
+      ></List>
+    );
+  }
 
-	render(){
-		return (
-			<div className="board">
-				<div className="container-fluid">
-					<div className="row">
-						{ this.state.lists.map( this.renderList , this)}
+  render() {
+    return (
+      <div className="board">
+        <div className="container-fluid">
+          <div className="row">
+            {this.state.lists.map(this.renderList, this)}
 
-						<div className="col-2">
-							<button type="button" className="btn btn-outline-light btn-block" onClick={ this.addList.bind(this) }>Add list</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+            <div className="col-2">
+              <button
+                type="button"
+                className="btn btn-outline-light"
+                onClick={this.addList.bind(this)}
+              >
+                Add list
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Board;
